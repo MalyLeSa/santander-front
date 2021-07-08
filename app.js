7f61c77c029a1a712d894a5c3f3cf780c2f5cc62
MalyLeSa <>
2021-07-07 21:56:29 -0500

    Extensión final

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const forms = document.querySelectorAll(".signup-form")
+const $forms = document.querySelectorAll(".signup-form");
+console.log($forms);
 
-const getTemplate = () => {
-    return fetch("./template.html").then((response) => response.text());
-  };
-  
-  const sendEmailToApi = (address, template) => {
-    fetch(`https://bedu-email-sender-api.herokuapp.com/send`, {
+const getTemplate = () =>{
+    return fetch("./template.html")
+    .then((response)=> response.text());
+}
+const sendEmailToApi = (address, template) => {
+    fetch(`https://bedu-email-sender-api.herokuapp.com/send?id`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -16,9 +17,13 @@ const getTemplate = () => {
       }),
     })
       .then((results) => {
-        console.log(results);
+        console.log(results.status);
+        if(results.status == 200){
+          alert("E-mail send!!!")
+        } else {
+          alert("Send failed")
+        }
         document.getElementById("email").value = ""
-        alert("E-mail send!!!")
       })
       .catch((error) => {
         console.error(error);
@@ -26,20 +31,22 @@ const getTemplate = () => {
         alert("Send failed")
       });
   };
-  
-  const sendEmail = (event) => {
+
+const sendEmail = (event)=>{
     event.preventDefault();
     const email = event.target.querySelector("input").value;
+    console.log(email);
     getTemplate()
-      .then((template) => {
-        sendEmailToApi(email, template);
-      })
-      .catch((error) => {
-        console.log(error, "error al convertir el template.");
-      });
-  };
-    
-for (let i = 0; i < forms.length; i++) {
-    forms[i].addEventListener("submit", sendEmail);
-  }
-  
+        .then((template)=>{
+            sendEmailToApi(email,template)
+            console.log(template);
+        })
+        .catch((error)=>{
+            console.log(error, "error al obtener template");
+        })
+}
+
+for(let i = 0;i < $forms.length; i++){
+    console.log($forms[i]);
+    $forms[i].addEventListener("submit", sendEmail);
+}
